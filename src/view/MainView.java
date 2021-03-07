f094cdbe26fe45238fca37d1f8a41c113dd41518
kyba <kyba@ozon.ru>
2021-03-08 01:26:02 +0300

    add main view form

---
+++ src/view/MainView.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class MainView {
+    private JTextArea TextForm;
+    private JPanel panel1;
+    private JFormattedTextField SaveButton;
+    private JFormattedTextField OpenButton;
+}
