7976f0be3289c3a740ad2baab62c3d41d6f106e3
Влад-Беляков <vladbelyakovv@gmail.com>
2021-03-08 00:44:42 +0300

    1 коммит: не умеет записывать

---
+++ src/Notepad/Vlad/com/Creators.java
@@ -0,0 +1,13 @@
+package Notepad.Vlad.com;
+
+import java.io.IOException;
+
+public abstract class Creators {
+
+    protected abstract boolean check();
+
+    protected abstract void creat(String path) throws IOException;
+
+    protected abstract void goBack() throws IOException;
+}
+
---
+++ src/Notepad/Vlad/com/NotepadBuilder.java
@@ -0,0 +1,11 @@
+package Notepad.Vlad.com;
+
+import java.io.IOException;
+
+public class NotepadBuilder {
+    public static void main(String[] args) throws IOException {
+        ClientWork c = new ClientWork();
+        c.commandsNotice();
+        c.clientWorking();
+    }
+}
